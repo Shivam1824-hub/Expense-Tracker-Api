5990442ba63fce76fd8d8fcc2536a291cddf63a0
Shivam <shivamchoudhary1824@gmail.com>
2026-09-13 13:46:17 +0530

    adding function to RequestDto

--- src/main/java/com/practice/intern/ET/Expense/Tracker/API/dto/ExpenseRequestDto.java
+++ src/main/java/com/practice/intern/ET/Expense/Tracker/API/dto/ExpenseRequestDto.java
@@ -1,5 +1,19 @@
 package com.practice.intern.ET.Expense.Tracker.API.dto;
 
-public class ExpenseRequestDto {
+import lombok.Data;
+import java.math.BigDecimal;
 
+@Data
+public class ExpenseRequestDto {
+    private String item;
+    private String category;
+    private double quantityValue;
+    private BigDecimal amount;
+
+    public ExpenseRequestDto(String item, String category, double quantityValue, BigDecimal amount) {
+        this.item = item;
+        this.category = category;
+        this.quantityValue = quantityValue;
+        this.amount = amount;
+    }
 }

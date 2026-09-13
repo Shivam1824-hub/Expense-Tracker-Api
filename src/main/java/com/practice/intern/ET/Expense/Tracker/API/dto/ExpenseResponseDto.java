0fa2bb302e4b540138f4e51ea4f9dd8790d40b1e
Shivam <shivamchoudhary1824@gmail.com>
2026-09-13 13:42:59 +0530

    adding Lombok dependency and function to ResponseDto

--- src/main/java/com/practice/intern/ET/Expense/Tracker/API/dto/ExpenseResponseDto.java
+++ src/main/java/com/practice/intern/ET/Expense/Tracker/API/dto/ExpenseResponseDto.java
@@ -1,4 +1,21 @@
 package com.practice.intern.ET.Expense.Tracker.API.dto;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class ExpenseResponseDto {
+
+    private String item;
+    private String category;
+    private double quantityValue;
+    private BigDecimal amount;
+
+    public ExpenseResponseDto(String item, double quantityValue, String category, BigDecimal amount) {
+        this.item = item;
+        this.quantityValue = quantityValue;
+        this.category = category;
+        this.amount = amount;
+    }
 }

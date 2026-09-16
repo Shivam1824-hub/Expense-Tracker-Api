03c6ba5805d9bb6f17b90c24b6ef1dc2b698895c
Shivam <shivamchoudhary1824@gmail.com>
2026-09-16 21:38:58 +0530

    updating Category String to Category object category connected entity

--- src/main/java/com/practice/intern/ET/Expense/Tracker/API/Model/Expense.java
+++ src/main/java/com/practice/intern/ET/Expense/Tracker/API/Model/Expense.java
@@ -1,9 +1,6 @@
 package com.practice.intern.ET.Expense.Tracker.API.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
 
@@ -19,9 +16,12 @@ public class Expense {
     private Long id;
 
     private String item;
-    private String category;
+
     private double quantityValue;
 //    private enum quantityUnit {  LITRE("l"), KILOGRAMS("kg"), PACKET("p"); }
     private BigDecimal amount;
+
+    @ManyToOne
+    private Category category;
 
 }
